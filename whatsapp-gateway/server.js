44fb69a22890e13b75d0b704156e8c92a5c137ec
drorganicpharm-source <drorganicpharm@gmail.com>
2026-06-30 17:11:25 +0300

    Fix: remove webVersionCache to fix LID error

--- whatsapp-gateway/server.js
+++ whatsapp-gateway/server.js
@@ -155,8 +155,7 @@ const whatsapp = new Client({
             '--single-process',
             '--disable-gpu'
         ]
-    },
-    webVersionCache: { type: 'remote', remotePath: 'https://raw.githubusercontent.com/nicedayzhu/whatsapp-web.js-versions/main/html/2.2412.54.html' }
+    }
 });
 
 whatsapp.on('qr', async (qr) => {
